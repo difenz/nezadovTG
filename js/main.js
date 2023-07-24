a7d1d1de64f0808e14aec2f4b79cfa63e65e81c9
German Novikov <36189558+difenz@users.noreply.github.com>
2023-07-24 20:10:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -46,18 +46,34 @@ for (var i = 0; i < btns.length; i++) {
     });
 }
 
-const counter50 = document.getElementById("counter50");
-const counterBtn = document.querySelectorAll(".btnn");
-let result50 = 0;
+var btn = document.querySelectorAll("a.modal-button");
 
-counterBtn.forEach((el, index) => {
-    el.addEventListener('click', () => {
-        if(index === 0 && result50 > 0) {
-            result50--;
-        } else if(index === 1) {
-            result50++;
-        }
-        counter50.textContent = result50;
-        total = counter50.textContent * 50;
-    });
-});
+var modals = document.querySelectorAll(".modal");
+
+var spans = document.getElementsByClassName("close");
+
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+  };
+}
+
+for (var i = 0; i < spans.length; i++) {
+  spans[i].onclick = function () {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  };
+}
+
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal")) {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  }
+};
